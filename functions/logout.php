8d6fd98014311cdc4c8fda7602c7dae02462ef23
Kostas Lampropoulos <klampropoulos@gmail.com>
2019-01-06 17:19:12 +0200

    oauth for smesec

--- functions/logout.php
+++ functions/logout.php
@@ -4,10 +4,13 @@
 	//check googleplus disconnect
 	if ($_SESSION['GPTOKEN']){
 		header('Location: ../gpconnect.php?disconnect=1');
-	}else{
-		header('Location: ../index.php'); 
+		
+		
+	}	else{
+		header('Location: https://keycloak.smesec.eu/auth/realms/SMESEC/protocol/openid-connect/logout?redirect_uri=https://securityaware.me'); 
 		$_SESSION['AUTHENTICATION'] = "";
 		session_destroy();
+		
 	}
 
 
