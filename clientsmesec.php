ae6b46c3b2cb219a6400e549de9b5ef39fe09c70
Kostas Lampropoulos <klampropoulos@gmail.com>
2019-01-06 17:31:18 +0200

    oauth for smesec with saved file

--- clientsmesec.php
+++ clientsmesec.php
@@ -12,7 +12,7 @@ $provider = new \OpenIDConnectClient\OpenIDConnectProvider([
     'urlAuthorize'            => 'https://keycloak.smesec.eu/auth/realms/SMESEC/protocol/openid-connect/auth',
     'urlAccessToken'          => 'https://keycloak.smesec.eu/auth/realms/SMESEC/protocol/openid-connect/token',
     'urlResourceOwnerDetails' => 'https://keycloak.smesec.eu/auth/realms/SMESEC/protocol/openid-connect/userinfo',
-	'publicKey'               => 'file://key/public.key',
+	'publicKey'               => 'file://keys/smesecpublic.key',
 ],
     [
         'signer' => $signer
@@ -68,32 +68,32 @@ header('Location: all_course.php?project_id=2');
 
 function registerUser($connection, $email, $fname, $lname, $picture){
 
-	$result_check_mail_query = "SELECT id_user FROM tbl_users WHERE email_user = '".$email."'";	
+	$result_check_mail_query = "SELECT id_user FROM tbl_users WHERE email_user = '".$email."'";
 	$result = $connection->query($result_check_mail_query) or die("Error in query.." . mysqli_error($connection));
-		
+
 	if( ($result->num_rows) == 0 )
-	{	
+	{
 		$passw =  rand();
 		$results_register_query = "INSERT INTO tbl_users (name_user, surname_user, email_user, password_user, active_user,register_date,last_login_date, avatar_name, auth_type) VALUES ('".
 		$fname."', '".$lname."','".$email."',MD5('".$passw."'), 1, now(),now(), '". $picture."','SMESEC')";
 		$results_register = $connection->query($results_register_query) or die("Error in query.." . mysqli_error($connection));
-		
+
 		$query_select = "SELECT id_user FROM tbl_users WHERE email_user ='".$email."' ";
 		$result_select = $connection->query($query_select) or die("Error in query.." . mysqli_error($connection)) ;
 		$obj = mysqli_fetch_object($result_select);
 		//make him a SMESEC APPROVED
 		$query_user_role="INSERT INTO tbl_user_role(id_user, id_role) VALUES (".$obj->id_user.",5)";
 		$results_user_role = $connection->query($query_user_role) or die("Error in query.." . mysqli_error($connection));
-		
+
 	}else{
 		$obj = mysqli_fetch_object($result);
-		$query_update = "UPDATE `tbl_users` SET  `last_login_date` =  now()  WHERE `tbl_users`.`id_user` =".$obj->id_user;	
+		$query_update = "UPDATE `tbl_users` SET  `last_login_date` =  now()  WHERE `tbl_users`.`id_user` =".$obj->id_user;
 		$result_upd = $connection->query($query_update)  or die("Error in query.." . mysqli_error($connection));
 	}
 
 }
 function makeUserALoggedInUser($connection, $email){
-	$result_check_mail_query = "SELECT * FROM tbl_users WHERE email_user = '".$email."'";	
+	$result_check_mail_query = "SELECT * FROM tbl_users WHERE email_user = '".$email."'";
 	$result = $connection->query($result_check_mail_query) or die("Error in query.." . mysqli_error($connection));
 	$row = mysqli_fetch_array($result);
 	$_SESSION['AUTHENTICATION'] = true;
@@ -104,7 +104,7 @@ function makeUserALoggedInUser($connection, $email){
 	$_SESSION['LNAME'] = $row['surname_user'];
 	$_SESSION['UROLE'] = "";
 	$_SESSION['UROLE_ID'] = "";
-	$Select_user_role_query="SELECT tbl_role.name_role,tbl_role.id_role FROM tbl_role INNER JOIN tbl_user_role ON tbl_role.id_role=tbl_user_role.id_role WHERE tbl_user_role.id_user=". $row['id_user'];	
+	$Select_user_role_query="SELECT tbl_role.name_role,tbl_role.id_role FROM tbl_role INNER JOIN tbl_user_role ON tbl_role.id_role=tbl_user_role.id_role WHERE tbl_user_role.id_user=". $row['id_user'];
 	//$result_user_role = mysql_query($Select_user_role_query);
 	$result_user_role = $connection->query($Select_user_role_query)  or die("Error in query.. result_user_role" . mysqli_error($connection));
 	$count_roles=0;
@@ -112,14 +112,14 @@ function makeUserALoggedInUser($connection, $email){
 	while($row1 = $result_user_role->fetch_array()){
 		if($count_roles>0)
 		{
-			$_SESSION['UROLE'] .= "/".$row1[0];	
+			$_SESSION['UROLE'] .= "/".$row1[0];
 			$_SESSION['UROLE_ID'] .= "/".$row1[1];
 		}
 		else
 		{
-			$_SESSION['UROLE'] .= $row1[0];	
+			$_SESSION['UROLE'] .= $row1[0];
 			$_SESSION['UROLE_ID'] .= $row1[1];
-			
+
 		}
 		$count_roles++;
 	}
